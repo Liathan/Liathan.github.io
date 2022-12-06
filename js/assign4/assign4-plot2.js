d793611b8ee36bfd8df47b69c6b03b0c4373cf85
liathan <liathancorvinus@gmail.com>
2022-12-06 15:46:36 +0100

    Mini-Rafectoring + legenda

--- js/assign4/assign4-plot2.js
+++ js/assign4/assign4-plot2.js
@@ -24,24 +24,33 @@ var groupBy = function(xs, key) {
     };
 
 // Title
-// svg_2.append("text")
-//     .attr("x", ((width_2 - (width_2 + margin_2.left - margin_2.right)) / 2))             
-//     .attr("y", 0 - ((height_2 - margin_2.top) / 2))
-//     .style("class", "h2")
-//     .style("font-size", "18px")
-//     .attr("text-anchor", "middle")  
-//     .style("text-decoration", "underline")  
-//     .text("Density of trees in Trento's neighborhoods");
+svg_2.append("text")
+.attr("x", 0)             
+.attr("y", -20 )
+.style("class", "h2")
+.style("font-size", "18px")
+.attr("text-anchor", "left")  
+.style("text-decoration", "underline")
+.text("Average Temperature")
+.append("tspan")
+.attr("x", 0)
+.attr("dy", "1.4em")
+.text("by month per year");
 
-var pippo;
+color_2 = d3.scaleOrdinal().range(["#F00314", "#FF8019", "#FAE603", "#28E10A", "#3BB5FF", "#0500C7", "#5C03FA", "#DE00ED"])
+
 var years = new Set
 d3.csv("../../data/assign4/assign4-plot2.csv").then( function (data)
 {
     for(i = 0; i < data.length; ++i)
         years.add(data[i]['year'])    
-    var max = data.reduce((x, y) => Math.max(x, y.avg), -Infinity)
+    
     var names = ["January", "February", "March", "April", "May", "June",
-                "July", "August", "September", "October", "November", "December"]
+    "July", "August", "September", "October", "November", "December"]
+    
+    years = Array.from(years)
+
+    color_2.domain(years)
 
     var numAxis = 4
     var radius = height_2 / 2
@@ -51,13 +60,13 @@ d3.csv("../../data/assign4/assign4-plot2.csv").then( function (data)
 
     var axisGrid = svg_2.append('g').attr("class", "axisWrapper")
 
+    xCoord = (l,i) => scale(l) * Math.cos(angle * i - Math.PI / 2)
+    yCoord = (l,i) => scale(l) * Math.sin(angle * i - Math.PI / 2)
+
     axisGrid.selectAll(".levels")
     .data(d3.range(1, numAxis + 1))
     .join("circle")
-    .attr("r", function (d) {
-        console.log(d) 
-        return d / numAxis * radius;
-        })
+    .attr("r", (d) => d / numAxis * radius)
     .attr("cx", width_2 / 2)
     .attr("cy", height_2 / 2)
     .style("fill", "#CDCDCD")
@@ -80,8 +89,8 @@ d3.csv("../../data/assign4/assign4-plot2.csv").then( function (data)
     axis.append("line")
     .attr("x1", 0)
     .attr("y1", 0)
-    .attr("x2", (d, i) => scale(30 ) * Math.cos(angle * i - Math.PI / 2))
-    .attr("y2", (d, i) => scale(30 ) * Math.sin(angle * i - Math.PI / 2))
+    .attr("x2", (d, i) => xCoord(30, i))
+    .attr("y2", (d, i) => yCoord(30, i))
     .attr("class", "line")
     .style("stroke", "lightgrey")
     .style("stroke-width", "2px")
@@ -89,24 +98,39 @@ d3.csv("../../data/assign4/assign4-plot2.csv").then( function (data)
     axis.append("text")
     .attr("class", "text")
     .attr("text-anchor", "middle")
-    .attr("x", (d, i) => scale(30) *1.15 * Math.cos(angle * i - Math.PI / 2))
-    .attr("y", (d, i) => scale(30) *1.15 * Math.sin(angle * i - Math.PI / 2))
+    .attr("x", (d, i) => xCoord(30, i) * 1.15)
+    .attr("y", (d, i) => yCoord(30, i) * 1.15)
     .text(d => d)
     
     byYear = groupBy(data, "year")
 
-    fooX = (l,i) => scale(l) * Math.cos(angle * i - Math.PI / 2)
-    fooY = (l,i) => scale(l) * Math.sin(angle * i - Math.PI / 2)
-    years = Array.from(years)
     years.forEach( yearEl => 
         svg_2.append("g")
         .attr("transform", `translate(${width_2 / 2}, ${height_2 / 2})`)
         .append("polygon")
         .data( [ byYear[yearEl] ] )
-        .attr("points", d => d.reduce((acc, el) => acc + fooX(el.avg, (el.month -1)) + "," + fooY(el.avg, (el.month -1))+" ", ""))
-        .attr("stroke", (d => color(years.indexOf(yearEl))))
+        .attr("points", d => d.reduce((acc, el) => acc + xCoord(el.avg, (el.month -1)) + "," + yCoord(el.avg, (el.month -1))+" ", ""))
+        .attr("stroke", (d => color_2(yearEl)))
         .attr("stroke-width", "3px")
-        .attr("fill", (d => color(years.indexOf(yearEl))))
+        .attr("fill", (d => color_2(yearEl)))
         .attr("fill-opacity", 0.05)
     )
+
+    var legend_2 = svg_2.append("g")
+    .selectAll(".legend2")
+    .data(years)
+
+    legend_2.join("rect")
+    .attr("x", 0)
+    .attr("y", (d,i) => 30 + i * 25)
+    .attr("width", 20)
+    .attr("height", 20)
+    .attr("fill", (d) => color_2(d))
+
+    legend_2.join("text")
+    .attr("x", 22)
+    .attr("y", (d,i) => 30 + i * 25)
+    .text(d => d)
+    .attr("dy", 15)
+    .style("alignment-baseline", "middle")
 })
